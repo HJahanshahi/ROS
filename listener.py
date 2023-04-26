6afa8d50ac8e7739e661e56abce3eb2c134181b6
Hadi Jahanshahi <128206423+HJahanshahi@users.noreply.github.com>
2023-04-26 19:13:38 -0400

    Update listener.py

--- listener.py
+++ listener.py
@@ -1,9 +1,7 @@
-#!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
 
 def chatter_callback(message):
-    #get_caller_id(): Get fully resolved name of local node
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", message.data)
     
 def listener():
@@ -11,7 +9,6 @@ def listener():
 
     rospy.Subscriber("chatter", String, chatter_callback)
 
-    # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
 if __name__ == '__main__':
